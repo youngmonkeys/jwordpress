bbda7cbc03dd5f6c8e8a198c3354df8a909cae6d
Dung Ta Van <itprono3@gmail.com>
2023-04-01 14:35:39 +0700

    update

--- src/main/java/org/youngmonkeys/jwordpress/repo/WpTermRepository.java
+++ src/main/java/org/youngmonkeys/jwordpress/repo/WpTermRepository.java
@@ -2,6 +2,7 @@ package org.youngmonkeys.jwordpress.repo;
 
 import com.tvd12.ezydata.database.EzyDatabaseRepository;
 import com.tvd12.ezyfox.database.annotation.EzyRepository;
+import com.tvd12.ezyfox.util.Next;
 import org.youngmonkeys.jwordpress.entity.WpTerm;
 
 import java.math.BigInteger;
@@ -10,5 +11,8 @@ import java.util.List;
 @EzyRepository
 public interface WpTermRepository extends EzyDatabaseRepository<BigInteger, WpTerm> {
 
-    List<WpTerm> findByTermIdGt(BigInteger idExclusive);
+    List<WpTerm> findByTermIdGt(
+        BigInteger idExclusive,
+        Next next
+    );
 }
