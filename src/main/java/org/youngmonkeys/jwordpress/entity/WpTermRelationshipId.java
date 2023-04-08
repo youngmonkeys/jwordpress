c9d5f250238cb3066b1d21fb0446a243006d2cb4
Dung Ta Van <itprono3@gmail.com>
2023-04-08 09:50:53 +0700

    update WpTermRelationshipId

--- src/main/java/org/youngmonkeys/jwordpress/entity/WpTermRelationshipId.java
+++ src/main/java/org/youngmonkeys/jwordpress/entity/WpTermRelationshipId.java
@@ -1,14 +1,14 @@
 package org.youngmonkeys.jwordpress.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import java.io.Serializable;
 import java.math.BigInteger;
 
-@Getter
-@Setter
-@ToString
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class WpTermRelationshipId implements Serializable {
