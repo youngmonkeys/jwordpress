bc17ec53c71d5e5302a14d493dba07b59a6b067a
Dung Ta Van <itprono3@gmail.com>
2023-04-08 11:19:55 +0700

    update

--- src/main/java/org/youngmonkeys/jwordpress/repo/WpUserMetaRepository.java
+++ src/main/java/org/youngmonkeys/jwordpress/repo/WpUserMetaRepository.java
@@ -3,12 +3,24 @@ package org.youngmonkeys.jwordpress.repo;
 import com.tvd12.ezydata.database.EzyDatabaseRepository;
 import com.tvd12.ezyfox.database.annotation.EzyRepository;
 import org.youngmonkeys.jwordpress.entity.WpUserMeta;
+import org.youngmonkeys.jwordpress.entity.WpUserMeta;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 @EzyRepository
 public interface WpUserMetaRepository extends EzyDatabaseRepository<BigInteger, WpUserMeta> {
 
     List<WpUserMeta> findByUserId(BigInteger userId);
+
+    Optional<WpUserMeta> findByMetaKeyAndMetaValue(
+        String metaKey,
+        String metaValue
+    );
+
+    Optional<WpUserMeta> findByUserIdAndMetaKey(
+        BigInteger postId,
+        String metaKey
+    );
 }
