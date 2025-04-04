0c7321821fd0a21475edc80f9c8d1c8f1789b7e5
Dung Ta Van <itprono3@gmail.com>
2025-04-04 21:10:19 +0700

    add findByObjectIdGtOrEqualAndTermTaxonomyIdGt

--- src/main/java/org/youngmonkeys/jwordpress/repo/WpTermRelationshipRepository.java
+++ src/main/java/org/youngmonkeys/jwordpress/repo/WpTermRelationshipRepository.java
@@ -1,11 +1,25 @@
 package org.youngmonkeys.jwordpress.repo;
 
 import com.tvd12.ezydata.database.EzyDatabaseRepository;
+import com.tvd12.ezyfox.database.annotation.EzyQuery;
 import com.tvd12.ezyfox.database.annotation.EzyRepository;
 import org.youngmonkeys.jwordpress.entity.WpTermRelationship;
+import org.youngmonkeys.jwordpress.entity.WpTermRelationshipId;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @EzyRepository
-public interface WpTermRelationshipRepository extends EzyDatabaseRepository<BigInteger, WpTermRelationship> {
+public interface WpTermRelationshipRepository
+    extends EzyDatabaseRepository<WpTermRelationshipId, WpTermRelationship> {
+
+    @EzyQuery(
+        "SELECT e FROM WpTermRelationship e " +
+            "WHERE e.objectId > ?0 " +
+            "OR (e.objectId = ?0 AND e.termTaxonomyId > ?1)"
+    )
+    List<WpTermRelationship> findByObjectIdGtOrEqualAndTermTaxonomyIdGt(
+        BigInteger objectIdInclusive,
+        BigInteger termTaxonomyId
+    );
 }
